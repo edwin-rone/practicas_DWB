928702cf4188213f66f16c946d00b48aec798e87
edwin-rone <edwin_rone@ciencias.unam.mx>
2025-03-20 00:34:29 -0600

    Práctica 5 terminada

--- product/src/main/java/com/product/api/repository/RepoCategory.java
+++ product/src/main/java/com/product/api/repository/RepoCategory.java
@@ -3,13 +3,46 @@ package com.product.api.repository;
 import java.util.*;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.product.api.entity.Category;
+
+import jakarta.transaction.Transactional;
 
 @Repository
 public interface RepoCategory extends JpaRepository<Category, Integer> {
 	@Query(value = "SELECT * FROM Category ORDER BY category_id", nativeQuery = true)
 	List<Category> getCategories();
+	
+	@Query(value ="SELECT * FROM Category WHERE status = 1 ORDER BY category_id", nativeQuery = true)
+	List<Category> getActiveCategories();
+	
+	@Modifying
+	@Transactional
+	@Query(value = "INSERT INTO Category(category, tag, status) VALUES (:category, :tag, 1)", nativeQuery = true)
+	void createCategory(String category, String tag);
+	
+	@Modifying
+	@Transactional
+	@Query(value ="UPDATE Category SET category = :category, tag = :tag WHERE category_id = :category_id",nativeQuery = true)
+	void updateCategory(@Param("category_id") Integer category_id, @Param("category") String category,@Param("tag") String tag);
+	
+	@Modifying
+	@Transactional
+	@Query(value = "UPDATE Category SET status = 1 WHERE category_id = :category_id;", nativeQuery = true)
+	void enableCategory(@Param("category_id") Integer category_id);
+
+	@Modifying
+	@Transactional
+	@Query(value = "UPDATE Category SET status = 0 WHERE category_id = :category_id;", nativeQuery = true)
+	void disableCategory(@Param("category_id") Integer category_id);
+	
+	@Query(value = "SELECT * FROM Category WHERE category_id = :category_id ORDER BY category", nativeQuery = true)
+	Category getCategory(@Param("category_id") Integer category_id);
+
 }
+
+
--- product/src/main/java/com/product/api/service/SvcCategory.java
+++ product/src/main/java/com/product/api/service/SvcCategory.java
@@ -4,10 +4,16 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 
+import com.product.api.dto.DtoCategoryIn;
 import com.product.api.entity.Category;
+import com.product.commons.dto.ApiResponse;
 
 public interface SvcCategory {
 	
 	public ResponseEntity<List<Category>> getCategories();
-
+	public ResponseEntity<List<Category>> getActiveCategories();
+	public ResponseEntity<ApiResponse> createCategory(DtoCategoryIn in);
+	public ResponseEntity<ApiResponse> updateCategory(DtoCategoryIn in, Integer id);
+	public ResponseEntity<ApiResponse> enableCategory(Integer id);
+	public ResponseEntity<ApiResponse> disableCategory(Integer id);
 }
